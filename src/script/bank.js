996fb1f5ed4c4c6c6a02872bdccfc6f0a9548991
jahidulkanchan <mdj524156@gmail.com>
2024-09-22 17:09:08 +0600

    push

--- src/script/bank.js
+++ src/script/bank.js
@@ -26,16 +26,56 @@ document.getElementById("dpSubmitBtn").addEventListener("click", (e) =>{
     document.getElementById("totalBalance").innerText = totalBalanceParse + dpIdNumber;
     document.getElementById("dpIdNumber").value = '';
     document.getElementById("dpPinNumber").value = '';
+    // =============== Transaction History ===================
+
+    // ================== using innerText ================= 
+
+    // const historyDoc = document.createElement("h1");
+    // historyDoc.classList.add("mx-5", "flex" , "justify-between", "bg-green-500", "px-5", "items-center","mt-3", "py-3");
+    // historyDoc.innerText = `Deposite Amount - ${dpIdNumber}`
+    // document.getElementById("hsForm").appendChild(historyDoc);
+
+    // Using innerHTML ============== (Better then innerText =============)
+    // const historyDoc = document.createElement("div");
+    // historyDoc.innerHTML = `
+    // <h1 class="mx-5 flex items-center justify-between px-5 bg-green-500 mt-3 py-3">${dpIdNumber}<i class="text-2xl fa-solid fa-arrow-right-long"></i></h1>
+    // `
+    // document.getElementById('hsForm').appendChild(historyDoc);
+
+    // another Method using function ============================
+    transactionHistory("hsForm", dpIdNumber,"bg-green-500","fa-arrow-left-long");
   }
   else{
     alert("Error");
   }
-})
+});
+// withDrawButton =======================================
 document.getElementById("wdSubmitBtn").addEventListener("click", (e) =>{
   e.preventDefault();
+  let totalBalance = document.getElementById("totalBalance").innerText;
+  let totalBalanceParse = parseFloat(totalBalance);
   const wdAmountInput = getInputValue("wdAmountInput");
   const wdPinNumber = getInputValue("wdPinNumber");
-  console.log(wdAmountInput, wdPinNumber);
+
+  if(wdPinNumber === 1234){
+    document.getElementById("totalBalance").innerText = totalBalanceParse - wdAmountInput;
+    document.getElementById("wdAmountInput").value = '';
+    document.getElementById("wdPinNumber").value = '';
+
+    // =========== Transaction History ============
+    // const historyDoc = document.createElement("h1");
+    // historyDoc.classList.add("mx-5", "flex" , "justify-between", "bg-orange-300", "px-5", "items-center","mt-3", "py-3");
+    // historyDoc.innerText = `Withdraw Amount - ${wdAmountInput}`
+    // console.log(historyDoc.innerText)
+    // document.getElementById("hsForm").appendChild(historyDoc);
+
+    // another Method using function ============================
+    transactionHistory("hsForm", wdAmountInput , "bg-orange-300", "fa-arrow-right-long");
+  }
+  else{
+    alert("Error");
+  }
+  
 });
 
 
--- src/script/utilities.js
+++ src/script/utilities.js
@@ -13,3 +13,24 @@ const getInputValue = (id)=>{
   const getInputParseValue = parseFloat(getInputValueId);
   return getInputParseValue;
 }
+
+
+// transaction history  =======================================
+// const transactionHistory = (id, amountValue , classname)=>{
+//   const historyDoc = document.createElement("h1");
+//   historyDoc.classList.add("mx-5", "flex" , "justify-between", classname, "px-5", "items-center","mt-3", "py-3");
+//   historyDoc.innerText = `Deposite Amount - ${amountValue}`
+//   const output = document.getElementById(id).appendChild(historyDoc);
+//   return output;
+// }
+
+// ============== innerHTML is Better =============
+
+const transactionHistory = (id, amountValue,classname,icon)=>{
+  const historyDoc = document.createElement("div");
+  historyDoc.innerHTML = `
+  <h1 class="mx-5 flex items-center justify-between px-5 ${classname} mt-3 py-3">${amountValue}<i class="text-2xl fa-solid ${icon}"></i></h1>
+  `
+  const output = document.getElementById(id).appendChild(historyDoc);
+  return output;
+}
